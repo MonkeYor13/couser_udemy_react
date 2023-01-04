bc69fe6d2c116b2c82ad48fb5adacbaf21a37a77
“MonkeYor13” <monkeyor13@gmail.com>
2023-01-04 14:59:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// agregamos funciones
+function HolaMundo(example, edad) {
+	var present2 = (
+		<div>
+			<h2>Hola, estoy llamando a una {example}</h2>
+			<h3>tengo {edad} años</h3>
+		</div>
+	);
+
+	return present2;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// puedo llamar variables
+	var nombre = "Yorvis";
+	var present = <h2>Hola, Soy {nombre}</h2>;
+	var example = "funcion";
+	var edad = 39;
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				{/* puedeo agregar alERTAS, llamar objetos de HTML y funciones */}
+				{alert("hola soy yorvis")}
+				{present}
+				{HolaMundo(example, edad)}
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
