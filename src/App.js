113ff2c241f9c153ebb9f38b79b9155a09123c5f
“MonkeYor13” <monkeyor13@gmail.com>
2023-01-08 12:39:03 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./assets/imagenes/logo.svg";
+import "./assets/css/App.css";
 
 // agregamos funciones
 function HolaMundo(example, edad) {
