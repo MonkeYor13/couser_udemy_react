efd86b286d52223fafb63ce2d9cfb205d13449a4
“MonkeYor13” <monkeyor13@gmail.com>
2023-01-11 17:27:48 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,52 +1,11 @@
-import logo from "./assets/imagenes/logo.svg";
-import "./assets/css/App.css";
+import React from "react";
 
 //import de components files
-import MiComponente from "./components/MiComponente";
-import Peliculas from "./components/Pelicules";
-
-// agregamos funciones
-function HolaMundo(example, edad) {
-	var present2 = (
-		<div>
-			<h2>Hola, estoy llamando a una {example}</h2>
-			<h3>tengo {edad} años</h3>
-		</div>
-	);
- 
-	return present2;
-}
+import Header from "./components/Header";
 
 function App() {
-	// puedo llamar variables
-	var nombre = "Yorvis";
-	var present = <h2>Hola, Soy {nombre}</h2>;
-	var example = "funcion";
-	var edad = 39;
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				{/* {alert("hola soy yorvis")} */}
-				{present}
-				{HolaMundo(example, edad)}
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-			<section>
-				<MiComponente />
-				<Peliculas />
-			</section>
-		</div>
+		<Header/>
 	);
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+// import Logo from "../assets/imagenes/logo.svg";
+
+class Header extends Component {
+	render() {
+		return (
+		<h1>Header</h1>
+		);
+	}
+}
+
+export default Header;
